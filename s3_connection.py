cf224151b45768905078f08d98b0ea237cdb4274
Daniel Harty <dan.john.harty@gmail.com>
2020-03-08 16:48:53 +0100

    s3 connection working

--- s3_connection.py
+++ s3_connection.py
@@ -19,37 +19,32 @@ def get_S3_credentials():
 RAW_IMAGES_BUCKET = aux.CONFIG_PARSER.get("aws_s3_parameters", "raw_images_bucket_name")
 CSV_FILE = aux.CONFIG_PARSER.get("aws_s3_parameters", "related_data_csv")
 CREDENTIALS = get_S3_credentials()
-s3 = get_s3_client(CREDENTIALS)
-con = CREDENTIALS['conn']
 
 
-def get_s3_keys(bucket):
-    """Get a list of keys in an S3 bucket."""
-    keys = []
-    resp = s3.list_objects_v2(Bucket=bucket)
-    for obj in resp['Contents']:
-        keys.append(obj['Key'])
-    return keys
+def get_aws_session(credentials=CREDENTIALS):
+    session = boto3.Session(
+        aws_access_key_id=credentials["aws_access_key_id"],
+        aws_secret_access_key=credentials["aws_secret_access_key"],
+    )
+    return session
 
 
-s3_resource = boto3.resource('s3',
-        aws_access_key_id=CREDENTIALS['aws_access_key_id'],
-        aws_secret_access_key= CREDENTIALS['aws_secret_access_key'],
-        aws_session_token= CREDENTIALS['aws_session_token'],
-        )
+def yield_items_in_bucket(bucket):
+    """
+    GENERATOR: Yields avalable obj metadata on each item in bucket
+    """
+    for obj in bucket.objects.all():
+        d = {
+            "key": obj.key,
+            "last_modified": obj.last_modified,
+            "meta": obj.meta,
+            "owner": obj.owner,
+            "size": obj.size,
+        }
 
-# s3_resource = boto3.resource('s3',
-#         aws_access_key_id=CREDENTIALS['aws_access_key_id'],
-#         aws_secret_access_key= CREDENTIALS['aws_secret_access_key'])
-
-# bucket = s3_resource.Bucket(S3_BUCKET)
-# s3_resource.Object(S3_BUCKET, CSV_FILE).download_file(CSV_FILE)
-
-
-#        region_name=CREDENTIALS['region'],
-
-#s3.list_objects_v2(Bucket="eb6259dddf801e581271aaa026bba894a77281f2b9bd51ad9db622675b3a9e35")
+        yield d
 
 
-bucket = s3_resource.Bucket("eb6259dddf801e581271aaa026bba894a77281f2b9bd51ad9db622675b3a9e35")
-bucket = s3_resource.Bucket(S3_BUCKET)
+session = get_aws_session(CREDENTIALS)
+s3 = session.resource("s3")
+bucket = s3.Bucket(RAW_IMAGES_BUCKET)
