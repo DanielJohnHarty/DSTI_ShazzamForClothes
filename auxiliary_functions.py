d41eec2d8a7f8142bd27408ce3aea9140472b51d
Daniel Harty <dan.john.harty@gmail.com>
2020-03-08 17:08:36 +0100

    BUGFIX_auxiliary_functions call order

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -1,10 +1,11 @@
 import configparser
 
-CONFIG_PARSER = get_config_parser()
-
 
 def get_config_parser(config_file="config.ini"):
     # ConfigParser to read config.ini
     Config = configparser.ConfigParser()
     Config.read(config_file)
     return Config
+
+
+CONFIG_PARSER = get_config_parser()
