ce28cb662c423e003fb2bc89b4e07b40540ad41a
Daniel Harty <dan.john.harty@gmail.com>
2020-03-09 21:38:57 +0100

    BUGFIX - make test image paths relative

--- ImgPipeline/test_pre_process_incoming_file.py
+++ ImgPipeline/test_pre_process_incoming_file.py
@@ -1,11 +1,16 @@
+import os
 import pytest
 import pre_process_incoming_file as ppi
 
-test_csv = r"C:\Dev\DSTI_ShazzamForClothes\ImgPipeline\TestFiles\ex_export_data.csv"
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+test_csv = os.path.join(current_path, "TestFiles", "ex_export_data.csv")
+
 
 def test_yield_row_from_csv():
     gen_rows = ppi.yield_rows_from_csv(test_csv)
     assert len(list(gen_rows)) == 3
+
 
 def test_yield_row_from_csv2():
     gen_rows = ppi.yield_rows_from_csv(test_csv)
