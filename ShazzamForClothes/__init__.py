71990f5944ef8f81f3aeee5b7726c6b8e3d3ea47
Daniel Harty <dan.john.harty@gmail.com>
2020-03-13 09:54:15 +0100

    Removing unused s3 api

--- ShazzamForClothes/__init__.py
+++ ShazzamForClothes/__init__.py
@@ -1,6 +1,5 @@
 from .config import *
 from .database_api import *
 from .img_preprocessor import *
-from .s3_api import *
 
-__all__ = (database_api.__all__, img_preprocessor.__all__, s3_api.__all__, config.__all__)
+__all__ = (database_api.__all__, img_preprocessor.__all__, config.__all__)
--- ShazzamForClothes/s3_api/__init__.py
+++
@@ -1,2 +0,0 @@
-from .s3_connection import *
-__all__= (s3_connection.__all__)
